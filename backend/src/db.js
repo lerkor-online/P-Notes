bb78e1c6128e51f1caa4c7975bb004dfb370d749
lerkor-online <lerkor.online@gmail.com>
2023-12-31 14:05:36 -0300

    deploy backend

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,12 +4,17 @@ const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/notes`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, 
-  native: false, 
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+    }
+  }, 
 });
 const basename = path.basename(__filename);
 
